90e0b5d61521b54d889daa0740a632d5be820949
Pardeep Kumar <veervirvani@gmail.com>
2022-07-24 23:02:22 +0200

    Django Tutorial_PollsApp initial commit.

---
+++ polls/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+    # new modifications
+    latest_question_list = Question.objects.order_by('- pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def details(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
